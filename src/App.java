46c76abd8af256260ca5838fe1fb9cf8d83eacb8
Ярослав <108534787+pupa-9@users.noreply.github.com>
2023-10-08 22:26:51 +0400

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        try {
+            float number = InputUtils.getFloatInput();
+            System.out.println("Вы ввели число: " + number);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
