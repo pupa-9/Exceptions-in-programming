30e1ce673f92851ec4c0e783aa5d4f2b573c0231
Ярослав <108534787+pupa-9@users.noreply.github.com>
2023-10-08 22:28:17 +0400

    dz11

--- src/InputUtils.java
+++ src/InputUtils.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class InputUtils {
+    public static float getFloatInput() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Введите дробное число: ");
+            String inputStr = scanner.nextLine();
+            if (inputStr.isEmpty()) {
+                throw new Exception("Пустые строки вводить нельзя!");
+            }
+            try {
+                float input = Float.parseFloat(inputStr);
+                return input;
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка ввода! Попробуйте еще раз.");
+            }
+        }
+    }
+}
